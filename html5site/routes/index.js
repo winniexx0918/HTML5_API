0a78d53fef668f6a2d5949cdf31c3509bff7b8de
Winnie <winniexx.0918@gmail.com>
2019-10-03 16:23:48 +0800

    安裝、input標籤屬性、自訂驗證、audio_video影音控制。

---
+++ html5site/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '開始之前...' });
+});
+
+module.exports = router;
