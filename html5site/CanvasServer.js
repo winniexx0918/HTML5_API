1d370efe05424b0573c46fe3256be98a87876592
Winnie <winniexx.0918@gmail.com>
2019-10-14 17:15:09 +0800

    聊天室功能

---
+++ html5site/CanvasServer.js
@@ -0,0 +1,45 @@
+﻿var WebSocketServer = require('websocket').server;
+var http = require('http');
+var clients = [];
+var server = http.createServer(function (request, response) {
+
+});
+
+server.listen(8080, function () {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+});
+
+//每個連進來的使用者都會觸發request事件
+wsServer.on('request', function (request) {
+    var connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+
+    //將連進來的使用者存到clients陣列中
+    var index = clients.push(connection) - 1;
+
+    //瀏覽器傳過來得資料，會觸發message事件
+    //可以在此事件中接收資料
+    connection.on('message', function (message) {
+        console.log(message);
+        //將收到的資料，廣播給所有連進來的使用者
+        for (var i = 0, max = clients.length; i < max; i++) {      
+            clients[i].sendUTF(message.utf8Data);
+        }
+
+    });
+    // 瀏覽器關閉或離開會觸發close事件
+    connection.on('close', function (reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+
+
+
+
+
+
---
+++ html5site/Chat_Bootstrap_Server.js
@@ -0,0 +1,63 @@
+﻿//https://www.npmjs.com/package/websocket
+//https://gist.github.com/martinsik/2031681
+
+var WebSocketServer = require('websocket').server,
+    http = require('http'),
+    clients = [],  //記錄所有連線的client
+    users = [];    //記錄所有連線的使用者
+
+var server = http.createServer(function (request, response) {
+    
+});
+
+server.listen(8080, function () {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    
+});
+
+
+wsServer.on('request', function (request) {   
+    var connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    //console.log(connection);
+    clients.push(connection);
+
+    var userName = "guest"
+        index = 0;
+
+    connection.on('message', function (message) {
+        //console.log('Received Message: ' + message.utf8Data);
+
+         var data = JSON.parse(message.utf8Data);
+         var json = "";
+        if(data["name"] != null){
+            userName = data["name"]; 
+            console.log((new Date()) + userName + " 登入");
+            json= {time:(new Date()).getTime(),text: userName + " 登入",name:userName};
+        }else{
+            console.log((new Date()) + " " + userName + " 說：" +  data["message"]);
+
+            json = {time:(new Date()).getTime(),text:data["message"],name:userName};
+
+        }
+
+
+            //廣播給所有人
+            for(var i=0,max=clients.length;i<max;i++){
+              clients[i].sendUTF(JSON.stringify(json));
+            }
+        
+       
+
+        
+        //connection.sendUTF(JSON.stringify(users));
+    });
+    connection.on('close', function (reasonCode, description) {
+        users.splice(index, 1);  //splice(開始位置,刪除幾筆)刪除陣列中的元素
+        console.log((new Date()) + userName + connection.remoteAddress + ' disconnected.');
+    });
+});
